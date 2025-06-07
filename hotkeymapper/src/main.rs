08f63903d3b28b3ee0487f54efb58190d53c04be
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2025-06-07 16:34:01 +1200

    started on config file and keybind updates

--- hotkeymapper/src/main.rs
+++ hotkeymapper/src/main.rs
@@ -1,33 +1,48 @@
 mod windows;
 mod keybinds;
+
 use std::error::Error;
+use std::path::Path;
+use std::env;
+
 use eframe::egui;
 use regex::Regex;
 use winapi::shared::windef::HWND;
+use serde::{Deserialize, Serialize};
 
 struct App {
     applications: Vec<Application>,
     selected_application: Option<String>,
+    config_path: String,
 
-    keybinds: Vec<[&'static str; 4]>, // [ApplicationName, ExecutablePath+Arguments, Modifier, Key]
-    keybind_selected: String,
+    // Keybinds
+    keybinds: Vec<[String; 4]>, // [ApplicationName, ExecutablePath+Arguments, Modifier, Key]
+    keybind_selected: String,   
     keybind_selected_modifier: String,
     keybind_selected_key: String,
 
-    // Modal state
+    // Change Keybinds
     show_keybind_modal: bool,
     editing_app_name: String,
 
     regex: Vec<Regex>,
 }
+
 struct Application {
     handle: HWND,
     label: String,
     path: String,
 }
+
 struct Keybind {
-    application: String,
-    executable: String,
+    application: Application,
+    modifier: String,
+    key: String,
+}
+#[derive(Serialize, Deserialize)]
+struct ConfigKeybind {
+    application_label: String,
+    application_path: String,
     modifier: String,
     key: String,
 }
@@ -38,18 +53,54 @@ impl App {
         let mut app = Self {
             applications: Vec::new(),
             selected_application: None,
+            config_path: String::new(),
+            
             keybinds: Vec::new(),
-
             keybind_selected: String::new(),
             keybind_selected_modifier: String::new(),
             keybind_selected_key: String::new(),
 
-            // Modal state
             show_keybind_modal: false,
             editing_app_name: String::new(),
 
             regex: Vec::new(),
         };
+
+
+        /* Configuration File */
+
+        // Retrieve the File
+        app.config_path = match env::var("LOCALAPPDATA") {
+            Ok(local_path) => {
+
+                // Retrieve the Folder
+                let local_path = format!("{}\\Nancy Configuration", local_path.as_str());
+                let dir_path = Path::new(&local_path);
+                if !dir_path.exists() || !dir_path.is_dir() {
+                    create_config_path(&local_path).unwrap();
+                    local_path.to_string();
+                }
+
+                // Retrieve the File
+                let file_path_string = format!("{}\\config.json", local_path);
+                let file_path = Path::new(&file_path_string);
+                if !file_path.exists() || !file_path.is_file() {
+                    create_config_file(&file_path_string).unwrap();
+                    file_path_string
+                }
+                else { file_path_string }
+                
+            }
+            Err(error) => {
+                panic!("Unable to locate and create the configuration path. Error: {}", error);
+            }
+        };
+
+        // Retrieve the Keybinds
+        app.keybinds = load_config(&app.config_path).unwrap_or_default();
+
+        /* End Configuration File */
+
 
         // Regex
         app.regex.push(Regex::new(r"^(.*?) - (.*?) - Microsoft​ Edge$").unwrap());
@@ -61,11 +112,12 @@ impl App {
         if std::env::consts::OS == "windows" {
             if let Ok(applications) = windows::list_visible_windows(&app.regex) {
                 app.applications = applications.iter().map(|(handle, title, path)| Application { handle: *handle, label: title.to_string(), path: path.to_string() }).collect();
+                for application in &app.applications {
+                    if let Some(keybind) = app.keybinds.iter().find(|keybind| keybind[0] == application.label) {
+                    }
+                }
             }
         }
-
-        // Keybinds
-        app.keybinds.push(["Cursor", "cursor.exe", "Ctrl", "U"]);
 
         app
     }   
@@ -138,14 +190,17 @@ impl eframe::App for App {
                 /* Right Column */
                 egui::ScrollArea::vertical().id_salt("active_keybinds").show(&mut columns[1], |ui| {
 
+                    let mut refresh_windows = false;
+                    let mut refresh_keybinds = false;
+
                     // List of Associated Keybinds
                     for application in &self.applications {
                         
                         // Keybind Found
-                        if let Some(keybind) = self.keybinds.iter().find(|keybind| keybind[0] == &application.label) {
+                        if let Some(keybind) = self.keybinds.iter().find(|keybind| keybind[0] == application.label) {
                             
-                            let keybind_text = if keybind[2].is_empty() { format!("{}", keybind[3]) }       // Character Only
-                            else { format!("{} + {}", keybind[2], keybind[3]) };                                    // Modifier + Character
+                            let keybind_text = if keybind[2].is_empty() { format!("{}", keybind[3]) }               // Character Only
+                            else { format!("{} + {}", keybind[2], keybind[3]) };                                            // Modifier + Character
 
                             // Change Button
                             let button = egui::Button::new(&keybind_text);
@@ -156,13 +211,55 @@ impl eframe::App for App {
                                 self.keybind_selected = keybind[1].to_string();
                                 self.keybind_selected_modifier = keybind[2].to_string();
                                 self.keybind_selected_key = keybind[3].to_string();
+                                refresh_windows = true;
+                                refresh_keybinds = true;
                             }
                         }
 
+                        // No Keybind Found
                         else {
                             let button = egui::Button::new("~");
-                            ui.add_sized([20.0, 20.0], button.fill(egui::Color32::TRANSPARENT));
+                            let new_button = ui.add_sized([20.0, 20.0], button.fill(egui::Color32::TRANSPARENT));
+                            if new_button.clicked() {
+                                self.show_keybind_modal = true;
+                                self.editing_app_name = application.label.clone();
+                                refresh_windows = true;
+                                refresh_keybinds = true;
+                            }
                         }
+                    }
+
+                    // Handle Window Refresh
+                    if refresh_windows {
+                        if std::env::consts::OS == "windows" {
+                            match windows::list_visible_windows(&self.regex) {
+                                Ok(applications) => {
+                                    self.applications = applications.iter()
+                                        .filter_map(|(handle, title, path)| {
+                                            if title.trim().is_empty() {
+                                                None
+                                            }
+                                            else {
+                                                Some(Application { handle: *handle, label: title.to_string(), path: path.to_string() })
+                                            }
+                                        }).collect();
+                                }
+                                Err(e) => {
+                                    println!("Failed to refresh active windows. Error: {}", e);
+                                }
+                            }
+                        }
+                    }
+                    // Handle Keybind Refresh
+                    if refresh_keybinds {
+                        for application in &self.applications {
+                            if let Some(keybind) = self.keybinds.iter_mut().find(|keybind| keybind[0] == application.label) {
+                                keybind[1] = application.path.clone();
+                                keybind[2] = "".to_string();
+                                keybind[3] = "".to_string();
+                            }
+                        }
+                        save_config(&self.config_path, &self.keybinds).unwrap();
                     }
                 });
             });
@@ -201,10 +298,11 @@ impl eframe::App for App {
 
                         ui.horizontal(|ui| {
                             if ui.button("Save").clicked() {
-                                // Update the keybind
                                 if let Some(keybind) = self.keybinds.iter_mut().find(|kb| kb[0] == self.editing_app_name) {
-                                    keybind[2] = Box::leak(self.keybind_selected_modifier.clone().into_boxed_str());
-                                    keybind[3] = Box::leak(self.keybind_selected_key.clone().into_boxed_str());
+                                    keybind[2] = self.keybind_selected_modifier.clone();
+                                    keybind[3] = self.keybind_selected_key.clone();
+                                    save_config(&self.config_path, &self.keybinds).unwrap();
+
                                 }
                                 self.show_keybind_modal = false;
                             }
@@ -214,11 +312,53 @@ impl eframe::App for App {
                             }
                         });
                     });
-                });
+                }
+            );
         }
     }
 }
 
+/* Configuration File */
+fn create_config_path(config_path: &str) -> Result<(), Box<dyn Error>> {
+    std::fs::create_dir_all(config_path)?;
+    println!("Created the Configuration Folder Path");
+    Ok(())
+}
+fn create_config_file(config_file_path: &str) -> Result<(), Box<dyn Error>> {
+    std::fs::write(config_file_path, "")?;
+    println!("Created the Configuration File Path");
+    Ok(())
+}
+fn load_config(config_path: &str) -> Result<Vec<[String; 4]>, Box<dyn Error>> {
+    let file_contents = std::fs::read_to_string(config_path)?;
+    if file_contents.is_empty() { return Ok(Vec::new()); }
+
+    let config: Vec<ConfigKeybind> = serde_json::from_str(&file_contents)?;
+    let keybinds: Vec<[String; 4]> = config.iter().map(|keybind| [
+        keybind.application_label.clone(), 
+        keybind.application_path.clone(), 
+        keybind.modifier.clone(), 
+        keybind.key.clone()
+    ]).collect();
+
+    println!("Loaded the Configuration Keybinds");
+
+    Ok(keybinds)
+}
+fn save_config(config_path: &str, keybinds: &Vec<[String; 4]>) -> Result<(), Box<dyn Error>> {
+    let config: Vec<ConfigKeybind> = keybinds.iter().map(|keybind| ConfigKeybind {
+        application_label: keybind[0].clone(),
+        application_path: keybind[1].clone(),
+        modifier: keybind[2].clone(),
+        key: keybind[3].clone()
+    }).collect();
+
+    let encoded = serde_json::to_string(&config)?;
+    std::fs::write(config_path, encoded)?;
+
+    println!("Saved Current Keybinds to Configuration");
+    Ok(())
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let options = eframe::NativeOptions {
